916f01d8d9ecdfeee24d285346586f6ceaf3d1ed
mrsingh <mrsinghup52@gmail.com>
2025-06-20 03:47:27 +0530

    dynamic data in signup api

--- src/index.js
+++ src/index.js
@@ -1,37 +1,98 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const connectDB = require('./config/database');
 const User = require('./models/user')
+const connectDB = require("./config/database")
 
-app.post('/signup', async (req,res)=>{
-   // Creating a new instance of the User model
+app.use(express.json());
 
-  const user = new User({
-    firstName : "Adarsh",
-    lastName : "Singh",
-    emailId : "siddhu1146@gmail.com",
-    password : "siddhu  @123"
-  }) 
-   
-  try{
-    await user.save();
-    res.send("User added successfully!")
-  }catch(err){
-    console.error('something happen while signup', err.message)
-    res.status(500).send("Something went wrong while Sign up")
+app.post('/signup', async(req,res)=>{
+  console.log(req.body);
+  const user = new User(req.body)
+   try{
+    await user.save()
+    res.send('User log')
+    console.log("User Saved!")
   }
+  catch(err){
+    res.status(500).send('User not Saved due to Server ERROR')
+    console.log('User not Saved due to Server ERROR')
+  } 
 })
-
-
 
 connectDB()
-.then(()=>{
-    console.log('Database Connection Established!')
-
-    app.listen(3000,()=>{
-      console.log('Server Start Running on Port No.3000')
-    })
+try{
+  console.log('Database Connection Established!')
+  app.listen('3000',() => {
+  console.log("success port :", 3000 )
 })
-.catch((err)=>{
-  console.error('Server Error due to Database',err)
-})  
+} catch(err){
+  console.log('DB Connection Fault!', err)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express')
+// const app = express();
+// const connectDB = require('./config/database');
+// const User = require('./models/user')
+
+// app.post('/signup', async (req,res)=>{
+//    // Creating a new instance of the User model
+
+//   const user = new User({
+//     firstName : "Adarsh",
+//     lastName : "Singh",
+//     emailId : "siddhu1146@gmail.com",
+//     password : "siddhu  @123"
+//   }) 
+   
+//   try{
+//     await user.save();
+//     res.send("User added successfully!")
+//   }catch(err){
+//     console.error('something happen while signup', err.message)
+//     res.status(500).send("Something went wrong while Sign up")
+//   }
+// })
+
+
+
+// connectDB()
+// .then(()=>{
+//     console.log('Database Connection Established!')
+
+//     app.listen(3000,()=>{
+//       console.log('Server Start Running on Port No.3000')
+//     })
+// })
+// .catch((err)=>{
+//   console.error('Server Error due to Database',err)
+// })  
--- src/models/user.js
+++ src/models/user.js
@@ -1,27 +1,64 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const {Schema} = require('mongoose');
 
 const userSchema = new Schema({
-  firstName: {
-    type: String,
+  firstName : {
+    type : String
   },
-  lastName: {
-    type: String,
+  lastName : {
+    type : String
   },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String
+  Age : {
+    type : Number
   }
-});
+})
 
-const User = mongoose.model('User',userSchema); // you can also directly exports it -  module.exports = mongoose.model('User',userSchema);
+const User = mongoose.model("User",userSchema);
+module.exports = User
 
-module.exports = User;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require("mongoose");
+// const {Schema} = require('mongoose');
+
+// const userSchema = new Schema({
+//   firstName: {
+//     type: String,
+//   },
+//   lastName: {
+//     type: String,
+//   },
+//   emailId: {
+//     type: String,
+//   },
+//   password: {
+//     type: String,
+//   },
+//   age: {
+//     type: Number,
+//   },
+//   gender: {
+//     type: String
+//   }
+// });
+
+// const User = mongoose.model('User',userSchema); // you can also directly exports it -  module.exports = mongoose.model('User',userSchema);
+
+// module.exports = User;
